ecab0f856d8c961b1bf662639ef2af392bad1cee
Mary Monchari Gichana <marygichana95@gmail.com>
2024-09-24 13:07:29 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 
 // Write your code here
+let multiply;
+let num1 = 31;
+let num2 = 2;
+
+multiply = num1 * num2;
+
+multiply;
+
+
+let random;
+random =  20;
+Math.ceil(Math.random() *20)+1;
+
+random;
+
+let mod;
+let num3 = -10;
+let num4 = 3;
+
+mod = num3%num4 + 5 ;
+
+mod;
+
+let max;
+let set;
+set = [1,2,4,10,15,20];
+max = Math.max(1,2,4,10,15,20);
+
+max;
+
+
+
